b31b54c1eadd244e3b55ac3d0268ff252a4022b9
Bruno Paladino <brunos_paladino@hotmail.com>
2023-06-12 02:43:14 -0300

    Archivo JSON con productos

--- app.js
+++ app.js
@@ -3,34 +3,6 @@ let item;
 let more;
 let total;
 
-//funcion constructora para objetos personalizados
-function product(id, name, image, price, category, quantityInCart, pluralprice) {
-    this.id = id;
-    this.name = name;
-    this.image = image;
-    this.price = Number(price);
-    this.category = category;
-    this.quantityInCart = quantityInCart;
-    this.pluralprice = Number(pluralprice);
-}
-
-//objetos, los diferencio por categoria
-const product1 = new product("01-PikachuT", "Pikachu T-shirt","./Images/Pikachu_Tshirt.jpg", 17, "Pokemon", 0, 17);
-const product2 = new product("02-CharmanderT", "Charmander T-shirt","./Images/Charmander_Tshirt.jpg", 16, "Pokemon", 0, 16);
-const product3 = new product("03-BulbasaurT", "Bulbasaur T-shirt","./Images/Bulbasaur_Tshirt.jpg", 15, "Pokemon", 0, 15);
-const product4 = new product("04-SquirtleT", "Squirtle T-shirt","./Images/Squirtle_Tshirt.jpg", 16, "Pokemon", 0, 16);
-const product5 = new product("05-BatmanT", "Batman T-shirt","./Images/Batman_Tshirt.jpg", 17, "Heroes", 0, 17);
-const product6 = new product("06-SupermanT", "Superman T-shirt","./Images/Superman_Tshirt.jpg", 16, "Heroes", 0, 16);
-const product7 = new product("07-FlashT", "Flash T-shirt", 7500, "Heroes", 0, 0);
-const product8 = new product("08-PokeballH", "Pokeball Hoodie", 11000, "Pokemon", 0, 0);
-const product9 = new product("09-PikachuH", "Pikachu Hoodie", 12000, "Pokemon", 0, 0);
-const product10 = new product("10-BatmanH", "Batman Hoodie", 12000, "Heroes", 0, 0);
-const product11 = new product("11-RobinH", "Robin Hoodie", 10000, "Heroes", 0, 0);
-const product12 = new product("12-SupermanH", "Superman Hoodie", 11000, "Heroes", 0, 0);
-const product13 = new product("13-FlashH", "Flash Hoodie", 11500, "Heroes", 0, 0);
-//
-
-
 /* ELEMENTOS TRAIDOS CON DOM */
 //contenedor de productos
 const product_container = document.querySelector("#product_container");
@@ -51,7 +23,7 @@ const buttonConfirm = document.querySelector("#buttonConfirm");
 /* FIN ELEMENTOS TRAIDOS CON DOM */
 
 // Para el carrito de compras usamos un array, donde guardamos los productos que el comprador selecciona
-let cart = [];
+/* let cart = []; */
 
 // Variable donde guardamos los elementos del local storage
 let uploadCart = JSON.parse(localStorage.getItem("Products to buy"));
@@ -61,17 +33,12 @@ const products=[];
 
 
 /* INICIO DE FUNCIONES */
-/* FUNCION PARA PUSHEAR LOS PRODUCTOS A UN ARRAY(products) Y CARGARLOS A LA PAGINA  */
-function push(products,product1, product2, product3, product4,product5, product6){
-    products.push(product1, product2, product3, product4,product5, product6);
-    chargeProduct(products);
-}
-/* FIN FUNCION */
+/* FUNCION PARA TRAER LOS PRODUCTOS DEL ARCHIVO DATA.JSON, CARGAR LA TIENDA Y PONERLOS EN EL ARRAY DE PRODUCTOS */
+const askForProducts = async () => {
+    const resp = await fetch(`./data.json`);
+    const data = await resp.json();
 
-
-/* FUNCION PARA CARGAR PRODUCTOS A LA PAGINA */
-function chargeProduct(products){
-    products.forEach(product => {
+    data.forEach(product => {
         const charge = document.createElement("div");
         charge.classList.add("product");
         charge.innerHTML=
@@ -84,10 +51,11 @@ function chargeProduct(products){
             </div>
         `
         product_container.append(charge);
+        products.push(product);
     });
     chargeButtons();
 }
-/* FIN FUNCION */
+/* FIN DE FUNCION */
 
 
 /* FUNCION PARA ACTUALIZAR BOTONES DE COMPRA (y asignar evento) */
@@ -220,4 +188,4 @@ if (uploadCart == null){
     showcart();
 }
 
-push(products,product1, product2, product3, product4,product5, product6, product7);
+askForProducts();
